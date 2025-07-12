0d7e5266f7f711a59aea0e49ef302942b0d7ba80
Sunni <sunni@Sunnis-MacBook-Air.local>
2025-07-12 06:56:59 -0700

    Commit from Rstudio

---
+++ 01 - Read Preprocessing.R
@@ -0,0 +1,9 @@
+# 01 - Read Preprocessing
+## Samples have been converted from .bam to fastq.gz (no fastqc pr multiqc)
+
+
+## Set seed
+set.seed(123)
+## Load libraries 
+library(dada2)
+## Remove primers
