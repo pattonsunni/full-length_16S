1b31a38f1e12cf96e673169edf1630eb97ee1eab
Sunni <sunni@Sunnis-MacBook-Air.local>
2025-08-05 14:03:18 -0700

    Commit from Rstudio

--- 01 - Read Preprocessing.R
+++ 01 - Read Preprocessing.R
@@ -42,28 +42,98 @@ track <- filterAndTrim(path, filt, minQ=3, minLen=1000, maxN=0, rm.phix= FALSE, 
 derep <- derepFastq(filt)
 
 ## Learn errors ====
-err <- learnErrors(derep, BAND_SIZE=32, multithread=TRUE, errorEstimationFunction=dada2:::PacBioErrfun, nbases = 5e8) # band_size is for banded needleman-wunsh; default is 16. Increase if higher indels are expected
+err <- learnErrors(derep,multithread=TRUE, errorEstimationFunction=dada2:::PacBioErrfun, nbases = 5e8) # band_size is for banded needleman-wunsh; default is 16. Increase if higher indels are expected
 # The max qual score of 93 was not detected. Using standard error fitting.
 plotErrors(err) # doesn't look good 
 
+
+## Edits to learning errors (from github) ====
+loessErrfun_mod4 <- function(trans) {
+  qq <- as.numeric(colnames(trans))
+  est <- matrix(0, nrow=0, ncol=length(qq))
+  for(nti in c("A","C","G","T")) {
+    for(ntj in c("A","C","G","T")) {
+      if(nti != ntj) {
+        errs <- trans[paste0(nti,"2",ntj),]
+        tot <- colSums(trans[paste0(nti,"2",c("A","C","G","T")),])
+        rlogp <- log10((errs+1)/tot)  # 1 psuedocount for each err, but if tot=0 will give NA
+        rlogp[is.infinite(rlogp)] <- NA
+        df <- data.frame(q=qq, errs=errs, tot=tot, rlogp=rlogp)
+        
+        # original
+        # ###! mod.lo <- loess(rlogp ~ q, df, weights=errs) ###!
+        # mod.lo <- loess(rlogp ~ q, df, weights=tot) ###!
+        # #        mod.lo <- loess(rlogp ~ q, df)
+        
+        # jonalim's solution
+        # https://github.com/benjjneb/dada2/issues/938
+        mod.lo <- loess(rlogp ~ q, df, weights = log10(tot),degree = 1, span = 0.95)
+        
+        pred <- predict(mod.lo, qq)
+        maxrli <- max(which(!is.na(pred)))
+        minrli <- min(which(!is.na(pred)))
+        pred[seq_along(pred)>maxrli] <- pred[[maxrli]]
+        pred[seq_along(pred)<minrli] <- pred[[minrli]]
+        est <- rbind(est, 10^pred)
+      } # if(nti != ntj)
+    } # for(ntj in c("A","C","G","T"))
+  } # for(nti in c("A","C","G","T"))
+  
+  # HACKY
+  MAX_ERROR_RATE <- 0.25
+  MIN_ERROR_RATE <- 1e-7
+  est[est>MAX_ERROR_RATE] <- MAX_ERROR_RATE
+  est[est<MIN_ERROR_RATE] <- MIN_ERROR_RATE
+  
+  # enforce monotonicity
+  # https://github.com/benjjneb/dada2/issues/791
+  estorig <- est
+  est <- est %>%
+    data.frame() %>%
+    mutate_all(funs(case_when(. < X40 ~ X40,
+                              . >= X40 ~ .))) %>% as.matrix()
+  rownames(est) <- rownames(estorig)
+  colnames(est) <- colnames(estorig)
+  
+  # Expand the err matrix with the self-transition probs
+  err <- rbind(1-colSums(est[1:3,]), est[1:3,],
+               est[4,], 1-colSums(est[4:6,]), est[5:6,],
+               est[7:8,], 1-colSums(est[7:9,]), est[9,],
+               est[10:12,], 1-colSums(est[10:12,]))
+  rownames(err) <- paste0(rep(c("A","C","G","T"), each=4), "2", c("A","C","G","T"))
+  colnames(err) <- colnames(trans)
+  # Return
+  return(err)
+}
+
+# check what this looks like
+errF_4 <- learnErrors(
+  derep,
+  multithread = TRUE,
+  nbases = 1e10,
+  errorEstimationFunction = loessErrfun_mod4,
+  verbose = TRUE
+)
+
+
 ## Denoise ====
-denoise <- dada(derep, err=err, BAND_SIZE=32, multithread=TRUE)
+denoise <- dada(derep, err=errF_4, BAND_SIZE=32, multithread=TRUE)
 
 ## Make sequence table ====
 ### from here to remove chimeras, wasn't in the tutorial; they did assign taxonomy using the denoise object
 seq_table <- makeSequenceTable(denoise) 
 table(nchar(getSequences(seq_table))) 
-sum(seq_table) #1,403,308
+sum(seq_table) #1,398,080
 
 ### Keep contigs within desired size range
 seq_table<-seq_table[,nchar(colnames(seq_table)) %in% 1455:1481]
 table(nchar(getSequences(seq_table)))
-dim(seq_table) # only 277 contigs
-sum(seq_table) # 890,996 reads (lost around 500,000)
+dim(seq_table) # only 302 contigs
+sum(seq_table) # 891,039 reads (lost around 500,000)
 
-seq_table_nochim <- removeBimeraDenovo(seq_table, method="consensus", multithread=TRUE, verbose=TRUE) #Identified 21 bimeras out of 277 input sequences.
-dim(seq_table_nochim) # 256 contigs
-sum(seq_table) - sum(seq_table_nochim) # lost 14,618 reads
+seq_table_nochim <- removeBimeraDenovo(seq_table, method="consensus", multithread=TRUE, verbose=TRUE) #Identified 28 bimeras out of 302 input sequences.
+dim(seq_table_nochim) # 274 contigs
+sum(seq_table) - sum(seq_table_nochim) # lost 18,398 reads
 
 ## Assign taxonomy ====
 ### greengenes2
@@ -73,16 +143,18 @@ tax_silv <- assignTaxonomy(seq_table_nochim, "silva_nr99_v138.1_train_set.fa.gz"
 ### GTDB
 tax_gtdb <- assignTaxonomy(seq_table_nochim, "GTDB_bac120_arc122_ssu_r202_Genus.fa", multithread=TRUE) # campylobacterales still not classified beyond order
 ### RDP
-tax <- assignTaxonomy(seq_table_nochim, "rdp_19_toGenus_trainset.fa", multithread=TRUE) # says Nitratifractor in nautilales order 
+tax_rdp <- assignTaxonomy(seq_table_nochim, "rdp_19_toGenus_trainset.fa", multithread=TRUE) # says Nitratifractor in nautilales order 
 ### RefSeq
-tax <- assignTaxonomy(seq_table_nochim, "RefSeq_16S_6-11-20_RDPv16_fullTaxo.fa", multithread=TRUE) # campylobacterales still not classified beyond order
-
-
+tax_refseq <- assignTaxonomy(seq_table_nochim, "RefSeq_16S_6-11-20_RDPv16_fullTaxo.fa", multithread=TRUE) # campylobacterales still not classified beyond order
 
 
 saveRDS(seq_table_nochim, here::here("Output/01 - Read Preprocessing - Output/seq_table_nochim.rds"))
 saveRDS(tax_gg, here::here("Output/01 - Read Preprocessing - Output/tax_gg.rds"))
 saveRDS(tax_silv, here::here("Output/01 - Read Preprocessing - Output/tax_silv.rds"))
+saveRDS(tax_gtdb, here::here("Output/01 - Read Preprocessing - Output/tax_gtdb.rds"))
+saveRDS(tax_rdp, here::here("Output/01 - Read Preprocessing - Output/tax_rdp.rds"))
+saveRDS(tax_refseq, here::here("Output/01 - Read Preprocessing - Output/tax_refseq.rds"))
+
 
 
 
--- 03 - Phyloseq Preprocessing.R
+++ 03 - Phyloseq Preprocessing.R
@@ -7,6 +7,8 @@ set.seed(123)
 
 ## Load libraries ====
 library(phyloseq)
+library(speedyseq)
+library(microViz)
 
 ## Load metadata file ====
 readRDS(here::here("Output/02 - Metadata - Output/sample_df.rds")) -> sample_df
@@ -26,9 +28,90 @@ rownames(seq_table_nochim) <- x
 ps.All_gg <- phyloseq(otu_table(seq_table_nochim, taxa_are_rows=FALSE), sample_data(sample_df), tax_table(tax_gg))
 saveRDS(ps.All_gg, here::here("Output/03 - Phyloseq Preprocessing - Output/ps.All_gg.rds"))
 
+ps.All_silv <- phyloseq(otu_table(seq_table_nochim, taxa_are_rows=FALSE), sample_data(sample_df), tax_table(tax_silv))
+saveRDS(ps.All_silv, here::here("Output/03 - Phyloseq Preprocessing - Output/ps.All_silv.rds"))
+
+ps.All_gtdb <- phyloseq(otu_table(seq_table_nochim, taxa_are_rows=FALSE), sample_data(sample_df), tax_table(tax_gtdb))
+saveRDS(ps.All_gtdb, here::here("Output/03 - Phyloseq Preprocessing - Output/ps.All_gtdb.rds"))
+
+ps.All_refseq <- phyloseq(otu_table(seq_table_nochim, taxa_are_rows=FALSE), sample_data(sample_df), tax_table(tax_refseq))
+ps.All_rdp <- phyloseq(otu_table(seq_table_nochim, taxa_are_rows=FALSE), sample_data(sample_df), tax_table(tax_rdp))
+
+
 ## Inspect phyloseq object ====
-ps.All_gg # only 256 taxa somehow
+ps.All_silv # only 274 taxa somehow
 # Taxa distribution 
-summary(taxa_sums(ps.All_gg@otu_table)) 
+summary(taxa_sums(ps.All_silv@otu_table)) 
 # Sample read distribution
-summary(sample_sums(ps.All_gg@otu_table)) 
+summary(sample_sums(ps.All_silv@otu_table)) 
+# Remove any taxa not annotated to kingdom level 
+## gtdb has 2 ASVs not annotated to kingdom level; one is hitting to A. palmata and the other isn't hitting to anything
+sum(taxa_sums(ps.All_silv)) # 876378
+subset_taxa(ps.All_silv, Kingdom != "NA") -> ps.All_silv
+sum(taxa_sums(ps.All_silv)) # 872,641
+
+## Add ASV column to taxonomy table ====
+ps.All_silv <- ps.All_silv %>% mutate_tax_table(ASV = paste0("ASV", 1:274))
+
+## Save taxonomy table (only DNA sequence and ASV columns) ====
+### Save taxonomy table as tibble
+as_tibble(ps.All_silv@tax_table) -> taxa_tibble
+### Save only relevent columns
+taxa_df <- data.frame(taxa_tibble$.otu, taxa_tibble$ASV)
+### Rename columns
+colnames(taxa_df) <- c("Sequence", "ASV")
+
+readr::write_csv(taxa_df, here::here("Output/03 - Phyloseq Preprocessing - Output/sequenceASV_gtdb.csv"))
+
+## Fix taxonomy and add new taxonomy column ====
+tax_fix(
+  ps.All_silv,
+  min_length = 4,
+  unknowns = NA,
+  suffix_rank = "classified",
+  sep = " ",
+  anon_unique = TRUE,
+  verbose = TRUE
+) -> ps.All_silv
+
+mutate_tax_table(ps.All_silv, ASVnew = paste0(Genus, " ", ASV)) -> ps.All_silv
+
+
+## Relative abundance ====
+ps.All_silv.trans <- transform_sample_counts(ps.All_silv, function(OTU) OTU/sum(OTU))
+
+## Identify top 10 most abundant taxa
+top20 <- names(sort(taxa_sums(ps.All_silv.trans), decreasing = TRUE))[1:20] 
+
+## Prune top 10
+ps.rare.top20 <- prune_taxa(top20, ps.All_silv.trans)
+
+## Relative abundance ====
+
+
+plot.relAbund.all <- plot_bar(ps.rare.top20, x="Samples", fill="ASVnew") 
+
+plot.relAbund.all <- plot.relAbund.all + theme_bw() + 
+  theme(axis.text.x = element_text(angle = 90, face = "bold", size = 11.5), 
+        axis.text.y = element_text(face = "bold", size = 11.5), title = element_text(face = "bold")) + xlab("Samples") + labs(title = "Relative Abundance")
+
+
+
+## Subset only G50 samples ====
+subset_samples(ps.All_gtdb, Samples == "G50_T0_M1_3_b" | Samples == "G50_T0_M1_3_d") -> ps.G50_gtdb
+ps.G50_gtdb <- prune_taxa(taxa_sums(ps.G50_gtdb@otu_table) > 0, ps.G50_gtdb)
+
+ps.G50_gtdb.trans <- transform_sample_counts(ps.G50_gtdb, function(OTU) OTU/sum(OTU))
+
+## Identify top 10 most abundant taxa
+top10 <- names(sort(taxa_sums(ps.G50_gtdb.trans), decreasing = TRUE))[1:10] 
+
+## Prune top 10
+ps.rare.top10 <- prune_taxa(top10, ps.G50_gtdb.trans)
+
+plot.relAbund.all <- plot_bar(ps.rare.top10, x="Samples", fill="ASVnew") 
+
+plot.relAbund.all <- plot.relAbund.all + theme_bw() + 
+  theme(axis.text.x = element_text(angle = 90, face = "bold", size = 11.5), 
+        axis.text.y = element_text(face = "bold", size = 11.5), title = element_text(face = "bold")) + xlab("Samples") + labs(title = "Relative Abundance")
+
